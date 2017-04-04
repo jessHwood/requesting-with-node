2a90e0880dfae2c0595f62407ba0a40255b15a00
jessHwood <jhorntvedt@gmail.com>
2017-04-04 16:52:17 -0600

    prettified

--- movie.js
+++ movie.js
@@ -1,7 +1,9 @@
 function getMovie(movieTitle) {
 	var request = require('request');
 	request('https://www.omdbapi.com/?t=' + movieTitle, function(error, response, body){
-			console.log(body);
+		var parsed = JSON.parse(body);
+			console.log(parsed);
+			console.log('\n');
 	});
 }
 
